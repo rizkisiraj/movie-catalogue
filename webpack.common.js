fed34db3c041af1b85aef229c65c0148bd07ae77
rizkisiraj <jomirwa@gmail.com>
2023-01-25 14:06:20 +0700

    fixing dotenv bugs in webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
